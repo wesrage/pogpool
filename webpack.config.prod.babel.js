7bd3fe481598b0248e1a2720f688ef5def0cdc6e
Wesley Waldner <wesrage@gmail.com>
2017-04-10 01:21:07 -0600

    Set environment variables properly for prod build Set correct character encoding

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -5,11 +5,6 @@ export default {
    ...baseConfig,
    plugins: [
       ...baseConfig.plugins,
-      new webpack.DefinePlugin({
-         'process.env': {
-            NODE_ENV: JSON.stringify('production'),
-         },
-      }),
       new webpack.optimize.UglifyJsPlugin(),
    ],
 };

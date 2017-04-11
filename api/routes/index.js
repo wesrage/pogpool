ffa3cae998f529118fa401b48457b88eb8140c76
Wesley Waldner <wes@gignation.ca>
2017-04-10 19:59:38 -0600

    Update API host

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,5 @@
-import wildcard from './wildcard';
 import picks from './picks';
 
 export function createRoutes(app) {
    app.put('/picks', picks.PUT);
-   app.get('*', wildcard);
 }
--- api/routes/wildcard.js
+++
@@ -1,3 +0,0 @@
-export default function wildcard(req, res) {
-   res.json({ message: 'API response!' });
-}

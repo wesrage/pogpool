6df8ec2980168a9c58b7abc9d78fc6117411c62a
Wesley Waldner <wes@gignation.ca>
2017-08-22 23:34:59 -0600

    Add routes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Standings from './Standings';
+import Picker from './Picker';
 import Themes from '../common/themes';
 
 const App = () => (
    <ThemeProvider theme={Themes.PIT}>
-      <Standings/>
+      <Router>
+         <div>
+            <Route exact path="/" component={Standings}/>
+            <Route exact path="/picker" component={Picker}/>
+         </div>
+      </Router>
    </ThemeProvider>
 );
 
